e5d3c3c7e94e7ca2ec262ad6bf277a3c199d5e74
Hochikong <hochikong@foxmail.com>
2017-04-01 23:47:54 +0800

    finish the first version

---
+++ kickit.py
@@ -0,0 +1,24 @@
+import subprocess
+import ConfigParser
+
+# suspend the 8021x.exe process
+parser = ConfigParser.ConfigParser()
+parser.read("config.ini")
+tgtbit = parser.get("System", "version")
+tgtprocess = parser.get("Processes", "process")
+
+tmp = subprocess.Popen(
+    "tasklist|findstr 8021x.exe",
+    shell=True,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE)
+pid = (str(tmp.stdout.readlines()[0]).split())[1]
+
+if tgtbit == "64":
+    subprocess.Popen("PStools/pssuspend64.exe %s" % pid)
+else:
+    subprocess.Popen("PStools/pssuspend.exe %s" % pid)
+
+# restart VMware NAT service
+tgtservice = parser.get("Services", "service")
+subprocess.Popen("sc start %s" % tgtservice)
